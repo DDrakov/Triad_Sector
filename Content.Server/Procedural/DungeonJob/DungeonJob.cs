a4a857ff34e5657efbe66f10702cd6affed9cf5d
TemporalOroboros <TemporalOroboros@gmail.com>
2025-06-21 08:23:19 -0700

    Kills TurfHelpers (#37939)
    * Create TurfSystem equivalent for and obsolete TurfHelpers.GetTileRef
    
    * Fix EntitySystem uses of TurfHelpers.GetTileRef
    
    * Fix EntitySystem uses of TurfHelpers.TryGetTileRef
    
    * Fix construction condition uses of TurfHelpers.GetTileRef
    
    * Fix last use of TurfHelpers.IsBlockedTurf
    
    * Create TurfSystem equivalent to and obsolete TurfHelpers.GetContentTileDefinition
    
    * Fix uses of TurfHelpers.GetContentTileDefinition(TileRef)
    
    * Fix uses of TurfHelpers.GetContentTileDefinition(Tile)
    
    * Create TurfSystem equivalent to and obsolete TurfHelpers.IsSpace
    
    * Fix EntitySystem uses of TurfHelpers.IsSpace(Tile)
    
    * Fix EntitySystem uses of TurfHelpers.IsSpace(TileRef)
    
    * Fix remaining uses of TurfHelpers.IsSpace
    
    * Fix uses of TurfHelpers.GetEntitiesInTile
    
    * Delete TurfHelpers.cs
    
    * Add GetEntitiesInTile lookup methods
    
    * Convert some GetEntitiesInTile methods to LookupSystem extension methods
    
    * Use new GetEntitiesInTile methods
    
    * Recycle spiderweb hashset
    
    * Recycle floor tile hashset

--- Content.Server/Procedural/DungeonJob/DungeonJob.cs
+++ Content.Server/Procedural/DungeonJob/DungeonJob.cs
@@ -54,6 +54,7 @@ public sealed partial class DungeonJob : Job<List<Dungeon>>
     private readonly EntityTableSystem _entTable;
     private readonly TagSystem _tags;
     private readonly TileSystem _tile;
+    private readonly TurfSystem _turf;
     private readonly SharedMapSystem _maps;
     private readonly SharedTransformSystem _transform;
     private readonly TurfSystem _turf;
@@ -84,6 +85,7 @@ public sealed partial class DungeonJob : Job<List<Dungeon>>
         DungeonSystem dungeon,
         EntityLookupSystem lookup,
         TileSystem tile,
+        TurfSystem turf,
         SharedTransformSystem transform,
         DungeonConfig gen,
         MapGridComponent grid,
@@ -104,6 +106,7 @@ public sealed partial class DungeonJob : Job<List<Dungeon>>
         _dungeon = dungeon;
         _lookup = lookup;
         _tile = tile;
+        _turf = turf;
         _tags = _entManager.System<TagSystem>();
         _maps = _entManager.System<SharedMapSystem>();
         _entTable = _entManager.System<EntityTableSystem>();
