522582eeec22265dd0b7bdfa94764195586a9ea3
DDrakov <draggydrakov135@gmail.com>
2026-05-16 16:50:28 +0700

    Revert "Merge pull request #1 from DDrakov/Rebase"
    This reverts commit e0edcd9a047916ed2e09eb7fa440c0f11a098382, reversing
    changes made to 97fd37528d48c2d1dc96ddadfd5f1f8ba077d707.

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -1,4 +1,3 @@
-using Content.Client._Common.Consent; // Consent system
 using Content.Client.Administration.Managers;
 using Content.Client.Changelog;
 using Content.Client.Chat.Managers;
@@ -44,7 +43,6 @@ namespace Content.Client.Entry
 {
     public sealed class EntryPoint : GameClient
     {
-        [Dependency] private readonly IClientConsentManager _clientConsentManager = default!; // Consent system
         [Dependency] private readonly IBaseClient _baseClient = default!;
         [Dependency] private readonly IGameController _gameController = default!;
         [Dependency] private readonly IStateManager _stateManager = default!;
@@ -169,7 +167,6 @@ namespace Content.Client.Entry
             _overlayManager.AddOverlay(new RadiationPulseOverlay());
             _overlayManager.AddOverlay(new EmpBlastOverlay()); // Frontier
             _chatManager.Initialize();
-            _clientConsentManager.Initialize();
             _clientPreferencesManager.Initialize();
             _euiManager.Initialize();
             _voteManager.Initialize();
@@ -177,7 +174,6 @@ namespace Content.Client.Entry
             _userInterfaceManager.SetActiveTheme(_configManager.GetCVar(CVars.InterfaceTheme));
             _documentParsingManager.Initialize();
             _titleWindowManager.Initialize();
-
 
             _baseClient.RunLevelChanged += (_, args) =>
             {
